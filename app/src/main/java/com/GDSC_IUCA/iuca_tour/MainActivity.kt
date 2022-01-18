953e300298bafaa7dcf35cde1d2b0ac2ac03b83d
Azamat229 <azamatmambetaliev229@gmail.com>
2022-01-18 18:25:27 +0600

    UI language page

--- app/src/main/java/com/GDSC_IUCA/iuca_tour/MainActivity.kt
+++ app/src/main/java/com/GDSC_IUCA/iuca_tour/MainActivity.kt
@@ -2,10 +2,35 @@ package com.GDSC_IUCA.iuca_tour
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.my_toolbar))
+
+
+        // Navigation components
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView);
+        val navController = findNavController(R.id.fragmentContainerView)
+
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.firstFragment, R.id.secondFragment, R.id.thirdFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        bottomNavigationView.setupWithNavController(navController)
+
+
+
+
+        val config = AppBarConfiguration(navController.graph)
+        findViewById<Toolbar>(R.id.my_toolbar).setupWithNavController(navController, config)
+
+
     }
 }
