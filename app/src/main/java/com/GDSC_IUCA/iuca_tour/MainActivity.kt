3b0b58730525dfba3f44dc349aecf72f02368a52
Admin <azamatmambetalev229@gmail.com>
2022-01-24 19:09:13 +0600

    UI of start excurtion page almost done

--- app/src/main/java/com/GDSC_IUCA/iuca_tour/MainActivity.kt
+++ app/src/main/java/com/GDSC_IUCA/iuca_tour/MainActivity.kt
@@ -7,6 +7,9 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.denzcoskun.imageslider.ImageSlider
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.models.SlideModel
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -30,6 +33,20 @@ class MainActivity : AppCompatActivity() {
 
         val config = AppBarConfiguration(navController.graph)
         findViewById<Toolbar>(R.id.my_toolbar).setupWithNavController(navController, config)
+
+        val imageList = ArrayList<SlideModel>() // Create image list
+
+        // imageList.add(SlideModel("String Url" or R.drawable)
+        // imageList.add(SlideModel("String Url" or R.drawable, "title") You can add title
+
+        imageList.add(SlideModel(R.drawable.slider, ScaleTypes.FIT))
+        imageList.add(SlideModel(R.drawable.slider, ScaleTypes.FIT))
+        imageList.add(SlideModel(R.drawable.slider, ScaleTypes.FIT))
+        imageList.add(SlideModel(R.drawable.slider, ScaleTypes.FIT))
+        imageList.add(SlideModel(R.drawable.slider, ScaleTypes.FIT))
+
+        val imageSlider = findViewById<ImageSlider>(R.id.image_slider)
+        imageSlider.setImageList(imageList)
 
 
     }
