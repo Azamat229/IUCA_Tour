88f4d763cf2014ecb02943aae4e19e0d73f9f8bd
Azamat229 <azamatmambetaliev229@gmail.com>
2022-02-06 11:39:22 +0600

    In process animation of extend and callablse text

--- app/src/main/java/com/GDSC_IUCA/iuca_tour/OpeningFragment.kt
+++ app/src/main/java/com/GDSC_IUCA/iuca_tour/OpeningFragment.kt
@@ -8,7 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
 import com.GDSC_IUCA.iuca_tour.databinding.FragmentOpeningBinding
-
+import com.ms.square.android.expandabletextview.ExpandableTextView
 
 class OpeningFragment : Fragment(R.layout.fragment_opening) {
     private lateinit var binding: FragmentOpeningBinding
@@ -31,5 +31,10 @@ class OpeningFragment : Fragment(R.layout.fragment_opening) {
             activity?.startActivity(intent)
 
         }
+
+
+        // Get the id of the expandable text view and set the desired text of your choice.
+        val expTv: ExpandableTextView = binding.expandTextView
+        expTv.setText("Expandable Text View is an android library that allows the users to create the text view which can expand and collapse to read the text description. I bet you guys have seen this in a lot of android applications but might not know the name and its purpose. Well, below is a screenshot of the Instagram application on the Play store. This feature saves a lot of space, rather than laying out the huge chunks of information and occupying the entire page we can further use this option and can utilize the space")
     }
 }
